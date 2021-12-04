98f07b2ed96912e635827b066b1f5bb4bf00a26a
luke <davidderu1@mail.weber.edu>
2021-12-03 21:38:31 -0700

    added index and bikes page as well as scroll button. Components, trails, and deals still need to be done

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,8 +1,30 @@
 // Required by Webpack - do not touch
-require.context('../', true, /\.(html|json|txt|dat)$/i)
-require.context('../images/', true, /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/i)
-require.context('../stylesheets/', true, /\.(css)$/i)
+require.context("../", true, /\.(html|json|txt|dat)$/i);
+require.context("../images/", true, /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/i);
+require.context("../stylesheets/", true, /\.(css)$/i);
 
 //TODO - Your ES6 JavaScript code (if any) goes here
-import 'bootstrap'
+import "bootstrap";
 
+window.addEventListener("scroll", (event) => {
+  const button = document.getElementById("scroll-button");
+  if (button) {
+    const atBottom =
+      document.documentElement.scrollTop + window.innerHeight + 50 >=
+      document.body.scrollHeight;
+
+    if (atBottom) {
+      button.classList.remove("scroll-in");
+      button.classList.add("scroll-out");
+    } else {
+      button.classList.remove("scroll-out");
+      button.classList.add("scroll-in");
+    }
+  }
+});
+
+function scrollDown() {
+  window.scrollTo(0, document.body.scrollHeight);
+}
+
+window.scrollDown = scrollDown;
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
-const path = require('path');
+const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: {
-    main: './src/javascripts/main.js'
+    main: "./src/javascripts/main.js",
   },
-  mode: 'development',
-  devtool: 'inline-source-map',
+  mode: "development",
+  devtool: "inline-source-map",
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    // static: path.join(__dirname, 'dist'), // Webpack 5
+    static: path.join(__dirname, "dist"), // Webpack 5
     historyApiFallback: true,
     open: true,
     compress: true,
-    port: 8080
+    port: 8080,
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'javascripts/[name].js',
-    // clean: true // Webpack 5
+    path: path.resolve(__dirname, "dist"),
+    filename: "javascripts/[name].js",
+    clean: true, // Webpack 5
+    publicPath: "",
   },
   module: {
     rules: [
@@ -26,31 +26,41 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      }, {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: ["css-loader", "sass-loader"],
+      },
+      {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
-      }, {
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+      },
+      {
         test: /\.(html|json|txt|dat|gif|jpg|png|svg|eot|ttf|woff|woff2)$/i,
-        use: [{
-          loader: 'file-loader',
-          options: { 
-            name: '[name].[ext]',
-            outputPath: (url, resourcePath, context) => {
-              return resourcePath.includes(`${path.sep}images${path.sep}`) ? `images/${url}` : url
-            }
-          }
-        }]
-      }
-    ]
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: (url, resourcePath, context) => {
+                return resourcePath.includes(`${path.sep}images${path.sep}`)
+                  ? `images/${url}`
+                  : url;
+              },
+            },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'stylesheets/[name].css',
-    })
-  ]
+      filename: "stylesheets/[name].css",
+    }),
+  ],
 };
